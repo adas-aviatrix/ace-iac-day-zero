61b9e8b9f3c7152377622a5f0f8e0ac4bd9ac981
adas-aviatrix <81318788+adas-aviatrix@users.noreply.github.com>
2021-08-19 21:15:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "aviatrix-system"
+    organization = "aviatrix-systems"
     workspaces {
       name = "ace-iac-day-zero"
     }
